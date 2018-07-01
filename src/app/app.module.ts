d1d71fe4230571161e8cc9b5b52a586230a0ff82
Ponmudi Natarajan <ponmudi.n@gmail.com>
2018-07-02 01:04:38 +0530

    SPA Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import {PromotionService} from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -47,7 +48,10 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     AppRoutingModule
   ],
 
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService
+  ],
 
   bootstrap: [AppComponent]
 })
